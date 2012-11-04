c1a833e929fcfc73107efa027af482bec192e9a9
Brian J Brennan <brianloveswords@gmail.com>
2012-11-04 14:17:01 -0500

    Exemptions class

---
+++ exemptions.js
@@ -0,0 +1,27 @@
+
+function Exemptions(array) {
+  if (!(this instanceof Exemptions))
+    return new Exemptions(array);
+  this._list = array.map(function (entry) {
+    if (typeof entry === 'string') {
+      entry = entry.replace(/\*/g, '.*?');
+      return RegExp('^' + entry + '$');
+    }
+    return entry;
+  });
+};
+
+Exemptions.check = function isExempt(whitelist, path) {
+  var i = whitelist.length;
+  while (i--) {
+    if (whitelist[i].test(path))
+      return true;
+  }
+  return false;
+};
+
+Exemptions.prototype.check = function check(path) {
+  return Exemptions.check.bind(null, this._list)(path);
+};
+
+module.exports = Exemptions;
---
+++ tests/exemptions.test.js
@@ -0,0 +1,20 @@
+const test = require('tap').test;
+const Exemptions = require('../exemptions');
+
+test('basic', function (t) {
+  const whitelist = Exemptions(['bacon']);
+  t.same(whitelist.check('ham'), false);
+  t.same(whitelist.check('cheese'), false);
+  t.same(whitelist.check('bacon'), true);
+  t.end();
+});
+
+test('advanced', function (t) {
+  const whitelist = Exemptions(['*bac*']);
+  t.same(whitelist.check('ham'), false, 'no ham');
+  t.same(whitelist.check('cheese'), false, 'no cheese');
+  t.same(whitelist.check('bacon'), true, 'yes bacon');
+  t.same(whitelist.check('bacos'), true, 'yes bacos');
+  t.same(whitelist.check('chewbacca'), true, 'yes chewbacca');
+  t.end();
+});
