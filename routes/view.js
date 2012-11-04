bfd39d4f7ac0b87942f5f6b578734c53f5a78fd0
Brian J Brennan <brianloveswords@gmail.com>
2012-11-04 13:08:41 -0500

    Rewrite view.js a little

--- routes/view.js
+++ routes/view.js
@@ -1,49 +1,76 @@
 const env = require('../env');
 const PHONE_NUMBER = env.get('twilioFrom');
+const ORIGIN = env.get('origin');
 
-function getCsrfToken(req) {
-  return req.session._csrf;
+function getCsrfToken(req) { return req.session._csrf }
+function getPhoneNumber() { return PHONE_NUMBER }
+function getAdmin(req) { return req.admin }
+function getFlash(req) { return req.flash() }
+function getOrigin(req) { return ORIGIN }
+
+function each(obj, fn) {
+  Object.keys(obj).forEach(function (k) {
+    fn(k, obj[k]);
+  });
+};
+function render(req, res) {
+  return function (path, obj) {
+    const base = {
+      csrf: getCsrfToken(req),
+      flash: getFlash(req),
+      phone: getPhoneNumber(req),
+      admin: getAdmin(req),
+      origin: getOrigin(),
+    };
+    each(obj, function (k, v) { base[k] = v });
+    return res.render(path, base);
+  }
 }
 
-exports.index = function index (req, res) {
-  res.render('index.html', {
-    page: 'home',
-    csrf: getCsrfToken(req),
-    flash: req.flash(),
+exports.login = function login (req, res) {
+  render(req, res)('login.html', {
+    page: 'login',
+  });
+};
 
+exports.index = function index (req, res) {
+  render(req, res)('index.html', {
+    page: 'home',
     users: req.users,
-    phone: PHONE_NUMBER,
   });
 };
 exports.announce = function announce (req, res) {
   res.render('announce.html', {
-    page: 'announce',
     csrf: getCsrfToken(req),
-    flash: req.flash(),
+    flash: getFlash(req),
+    phone: getPhoneNumber(req),
+    admin: getAdmin(req),
 
+    page: 'announce',
     subscribers: req.subscribers,
-    phone: PHONE_NUMBER,
   });
 };
 exports.subscribers = function subscribers (req, res) {
   res.render('subscribers.html', {
-    page: 'subscribers',
     csrf: getCsrfToken(req),
-    flash: req.flash(),
+    flash: getFlash(req),
+    phone: getPhoneNumber(req),
+    admin: getAdmin(req),
 
+    page: 'subscribers',
     subscribers: req.subscribers,
-    phone: PHONE_NUMBER,
   });
 };
 exports.user = function user(req, res) {
   const user = req.user;
   res.render('user.html', {
-    page: 'user',
     csrf: getCsrfToken(req),
-    flash: req.flash(),
+    flash: getFlash(req),
+    phone: getPhoneNumber(req),
+    admin: getAdmin(req),
 
+    page: 'user',
     user: user,
     messages: user.messages.reverse(),
-    phone: PHONE_NUMBER,
   });
 };
