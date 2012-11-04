977587e07de79d972eca354d719e5ed27a0e1de7
Brian J Brennan <brianloveswords@gmail.com>
2012-11-04 11:10:33 -0500

    Add phone number to layout.

--- routes/view.js
+++ routes/view.js
@@ -1,19 +1,25 @@
+const env = require('../env');
+const PHONE_NUMBER = env.get('twilioFrom');
+
 exports.index = function index (req, res) {
   res.render('index.html', {
     page: 'home',
     users: req.users,
+    phone: PHONE_NUMBER,
   });
 };
 exports.announce = function announce (req, res) {
   res.render('announce.html', {
     page: 'announce',
     subscribers: req.subscribers,
+    phone: PHONE_NUMBER,
   });
 };
 exports.subscribers = function subscribers (req, res) {
   res.render('subscribers.html', {
     page: 'subscribers',
     subscribers: req.subscribers,
+    phone: PHONE_NUMBER,
   });
 };
 exports.user = function user(req, res) {
@@ -21,6 +27,7 @@ exports.user = function user(req, res) {
   res.render('user.html', {
     page: 'user',
     user: user,
-    messages: user.messages.reverse()
+    messages: user.messages.reverse(),
+    phone: PHONE_NUMBER,
   });
 };
